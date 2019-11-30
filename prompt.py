17b3ecbf7481fdc39c8ab796ef7e69910e8a9b42
2a6m <maxime.bourguignon1996@gmail.com>
2019-11-30 14:09:45 +0100

    set attribute private

--- prompt.py
+++ prompt.py
@@ -15,9 +15,9 @@ from .color import colored_command, colored_error
 class Prompt():
 
     def __init__( self, name="Prompt Application", query="Input > ", debug=False ):
-        self.settings = dict()
-        self.database = dict()
-        self.running = True
+        self.__settings = dict()
+        self.__database = dict()
+        self.__running = True
 
         self.set( "app_name", name )
         self.set( "query", query )
@@ -31,38 +31,41 @@ class Prompt():
     # Methods to configure the internal behaviour
 
     def addApp( self, call, app, explanation ):
-        self.database[call] = { "app": app, "help": explanation }
+        self.__database[call] = { "app": app, "help": explanation }
 
     def set( self, name_setting, value ):
-        self.settings[name_setting] = str(value)
+        self.__settings[name_setting] = str(value)
 
     # Usefull applications for a prompt
 
     def __helpMenu( self, args ):
         print( "Help list\n{:-<20}".format(""))
-        for key in sorted( self.database.keys()):
-            print( "{cmd:<34} {msg}".format( cmd=colored_command( key ), msg=self.database[key]["help"] ))
+        for key in sorted( self.__database.keys()):
+            print( "{cmd:<34} {msg}".format( cmd=colored_command( key ), msg=self.__database[key]["help"] ))
+        return True
 
     def __settingMenu( self, args ):
         print( "Setting list\n{:-<20}".format(""))
-        for key in sorted( self.settings.keys()):
-            print( "{cmd:<34} {msg}".format( cmd=colored_command( key ), msg=self.settings[key] ))
+        for key in sorted( self.__settings.keys()):
+            print( "{cmd:<34} {msg}".format( cmd=colored_command( key ), msg=self.__settings[key] ))
+        return True
 
     def __exitMenu( self, args ):
-        self.running = False
+        self.__running = False
+        return True
 
     # Methods for the prompt routine
 
     def __execute( self, call ):
         call = call.split()
-        if call[0] in self.database.keys():
-            return self.database[call[0]]["app"]( call[1:] )
+        if call[0] in self.__database.keys():
+            return self.__database[call[0]]["app"]( call[1:] )
         else:
             print( colored_error( "Command not supported" ) + ", please execute command " + colored_command( "help" ) + " to see the help menu" )
             return False
 
     def __startup ( self ):
-        print( self.settings["app_name"] + "\n" )
+        print( self.__settings["app_name"] + "\n" )
         print( "execute command " + colored_command( "help" ) + " to see the help menu\n{:-<20}".format(""))
 
     def __exit( self ):
@@ -71,7 +74,10 @@ class Prompt():
 
     def run( self ):
         self.__startup()
-        while self.running:
-            text = input( self.settings["query"] )
-            self.__execute( text )
+        while self.__running:
+            text = input( self.__settings["query"] )
+            if !self.__execute( text ) and self.__settings["debug"]:
+                print( colored_error( "error while executing the query" ) + " : " + text )
+            else:
+                pass
         self.__exit()
